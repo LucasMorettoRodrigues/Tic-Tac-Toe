bcd727945cb57a53ddbba40f92cad9041a1620ab
Lucas <r.lucasmoretto@gmail.com>
2021-12-27 15:47:40 -0300

    Add functions

--- script.js
+++ script.js
@@ -1,47 +1,76 @@
-const signal = 'X'
-const a = []
-
-const units = document.querySelectorAll('#unit')
-const unitsArray = Array.from(units)
-
-unitsArray.forEach(unit => unit.addEventListener('click', () => play(unit)))
-
-function play(unit) {
-    let index = unitsArray.indexOf(unit)
-    if (a[index] !== signal && a[index] !== 'O') {
-        unit.textContent = signal
-        a[index] = signal
-        console.log(endGame(a))
-        computerPlay()
-    }  
-} 
-
-function computerPlay() {
-    let n = Math.floor(Math.random() * 9)
-    if (a[n] === signal || a[n] === 'O') {
-        computerPlay()
-    } else {
-        a[n] = "O"
-        unitsArray[n].textContent = 'O'
-    }
+const createPlayer = (name, marker) => {
+    return {name, marker}
 }
 
-function endGame(a) {
-    if (a[0] === signal && a[4] === signal && a[8] === signal) {
-        return 'You win'
-    } else if (a[2] === signal && a[4] === signal && a[6] === signal) {
-        return 'You win'
-    }
-    
-    for (let i = 0; i < 9; i++) {
-        if (a[i] === signal && a[i+3] === signal && a[i+6] === signal) {
-            return 'You win'
-        } else if (a[i] === signal && a[i+1] === signal && a[i+2] === signal) {
-            return 'You win'
-        } else if (a[i] === signal && a[i+1] === signal && a[i+2] === signal) {
-            return 'You win'
+const gameBoard = (() => {
+    let board = []
+    const units = Array.from(document.querySelectorAll('#unit'))
+
+    function computerPlay() {
+        let n = Math.floor(Math.random() * 9)
+        if (board[n] === 'O' || board[n] === 'X') {
+            computerPlay()
+        } else {
+            play(units[n], n)
         }
     }
-}
+
+    units.forEach((unit, index) => unit.addEventListener('click', () => play(unit, index)))
+
+    function play(unit, index) {
+        unit.style.pointerEvents = 'none'
+        console.log(game.currentPlayer.marker)
+        unit.textContent = game.currentPlayer.marker
+        board[index] = game.currentPlayer.marker
+        game.spots--
+        console.log(game.spots)
+        if (game.checkWinner()){
+            console.log(game.currentPlayer.name + ' win the game!!!')
+        } else if (game.checkDraw(game.spots)) {
+            console.log('Draw!!!')
+        } else {
+            game.nextPlayer()
+            if (game.currentPlayer.name === 'Robot') {
+                computerPlay()
+            }
+        }
+    }
+    return {board}
+})()
+
+const game = (() => {
+    const playerOne = createPlayer('Thomas', 'X')
+    const playerTwo = createPlayer('Robot', 'O')
+
+    let currentPlayer = playerOne
+    let spots = 9
+
+    function nextPlayer() {
+        this.currentPlayer === playerOne ? this.currentPlayer = playerTwo : this.currentPlayer = playerOne
+    }
+
+    function checkWinner() {
+        if ((gameBoard.board[0] === this.currentPlayer.marker && gameBoard.board[4] === this.currentPlayer.marker && gameBoard.board[8] === this.currentPlayer.marker)  ||
+            (gameBoard.board[2] === this.currentPlayer.marker && gameBoard.board[4] === this.currentPlayer.marker && gameBoard.board[6] === this.currentPlayer.marker)  ||
+            (gameBoard.board[0] === this.currentPlayer.marker && gameBoard.board[1] === this.currentPlayer.marker && gameBoard.board[2] === this.currentPlayer.marker) ||
+            (gameBoard.board[3] === this.currentPlayer.marker && gameBoard.board[4] === this.currentPlayer.marker && gameBoard.board[5] === this.currentPlayer.marker) ||
+            (gameBoard.board[6] === this.currentPlayer.marker && gameBoard.board[7] === this.currentPlayer.marker && gameBoard.board[8] === this.currentPlayer.marker) ||
+            (gameBoard.board[0] === this.currentPlayer.marker && gameBoard.board[3] === this.currentPlayer.marker && gameBoard.board[6] === this.currentPlayer.marker) ||
+            (gameBoard.board[1] === this.currentPlayer.marker && gameBoard.board[4] === this.currentPlayer.marker && gameBoard.board[7] === this.currentPlayer.marker) ||
+            (gameBoard.board[2] === this.currentPlayer.marker && gameBoard.board[5] === this.currentPlayer.marker && gameBoard.board[8] === this.currentPlayer.marker)) {
+            return true
+        }
+    }
+
+    function checkDraw(spots) {
+        if (spots === 0){
+            return true
+        } 
+    }
+
+    return {currentPlayer, nextPlayer, spots, checkWinner, checkDraw}
+})()
+
+
 
 
